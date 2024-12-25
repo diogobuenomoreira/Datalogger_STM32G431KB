a0170b20ad14f4dfb2bc75ea741c10e5d7bb5995
Diogo <diogobuenomoreira@hotmail.com>
2024-12-25 23:33:12 +0100

    Adjust main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -97,26 +97,33 @@ int main(void)
   MX_ADC1_Init();
   MX_USART2_UART_Init();
   MX_TIM2_Init();
-  MX_SPI1_Init();
   if (MX_FATFS_Init() != APP_OK) {
     Error_Handler();
   }
+  MX_SPI1_Init();
+  MX_TIM7_Init();
   /* USER CODE BEGIN 2 */
+  SD_Card_Open();
   HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED);
   HAL_Delay(200);
- // HAL_TIM_Base_Start(&htim2);
- // HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&adc_value, 1);
+  HAL_TIM_Base_Start(&htim2);
+  HAL_TIM_Base_Start_IT(&htim7);
+  HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&adc_value, 1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  process_SD_card();
-  while (1)
+  while(Get_ADC_Stop_Flag()== 0)
   {
+	 SD_Write_Task();
+  }
+  SD_Write_Task();
+  SD_Card_Close();
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
+
   /* USER CODE END 3 */
 }
 
@@ -167,20 +174,6 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
-#ifdef __GNUC__
-  /* With GCC, small printf (option LD Linker->Libraries->Small printf
-     set to 'Yes') calls __io_putchar() */
-int __io_putchar(int ch)
-#else
-int fputc(int ch, FILE *f)
-#endif /* __GNUC__ */
-{
-  /* Place your implementation of fputc here */
-  /* e.g. write a character to the UART2 and Loop until the end of transmission */
-  HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
-
-  return ch;
-}
 
 void process_SD_card( void )
 {
@@ -195,10 +188,10 @@ void process_SD_card( void )
     fres = f_mount(&FatFs, "", 1);    //1=mount now
     if (fres != FR_OK)
     {
-      printf("No SD Card found : (%i)\r\n", fres);
+    	DEBUG_PRINTF("No SD Card found : (%i)\r\n", fres);
       break;
     }
-    printf("SD Card Mounted Successfully!!!\r\n");
+    DEBUG_MESSAGE("SD Card Mounted Successfully!!!\r\n");
 
     //Read the SD Card Total size and Free Size
     FATFS *pfs;
@@ -209,17 +202,17 @@ void process_SD_card( void )
     totalSpace = (uint32_t)((pfs->n_fatent - 2) * pfs->csize * 0.5);
     freeSpace = (uint32_t)(fre_clust * pfs->csize * 0.5);
 
-    printf("TotalSpace : %lu bytes, FreeSpace = %lu bytes\n", totalSpace, freeSpace);
+    DEBUG_PRINTF("TotalSpace : %lu bytes, FreeSpace = %lu bytes\n", totalSpace, freeSpace);
 
     //Open the file
     fres = f_open(&fil, "datalogger.txt", FA_WRITE | FA_READ | FA_CREATE_ALWAYS);
     if(fres != FR_OK)
     {
-      printf("File creation/open Error : (%i)\r\n", fres);
+    	DEBUG_PRINTF("File creation/open Error : (%i)\r\n", fres);
       break;
     }
 
-    printf("Writing data!!!\r\n");
+    DEBUG_MESSAGE("Writing data!!!\r\n");
     //write the data
     f_puts("Welcome to Datalogger Project", &fil);
 
@@ -230,31 +223,31 @@ void process_SD_card( void )
     fres = f_open(&fil, "datalogger.txt", FA_READ);
     if(fres != FR_OK)
     {
-      printf("File opening Error : (%i)\r\n", fres);
+    	DEBUG_PRINTF("File opening Error : (%i)\r\n", fres);
       break;
     }
 
     //read the data
     f_gets(buf, sizeof(buf), &fil);
 
-    printf("Read Data : %s\n", buf);
+    DEBUG_PRINTF("Read Data : %s\n", buf);
 
     //close your file
     f_close(&fil);
-    printf("Closing File!!!\r\n");
+    DEBUG_MESSAGE("Closing File!!!\r\n");
 #if 0
     //Delete the file.
     fres = f_unlink(datalogger.txt);
     if (fres != FR_OK)
     {
-      printf("Cannot able to delete the file\n");
+    	DEBUG_MESSAGE("Cannot able to delete the file\n");
     }
 #endif
   } while( false );
 
   //We're done, so de-mount the drive
   f_mount(NULL, "", 0);
-  printf("SD Card Unmounted Successfully!!!\r\n");
+  DEBUG_MESSAGE("SD Card Unmounted Successfully!!!\r\n");
 }
 /* USER CODE END 4 */
 
